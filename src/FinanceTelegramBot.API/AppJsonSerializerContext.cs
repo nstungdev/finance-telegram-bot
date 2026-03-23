8376ab91ed99e92cc41f962ec6898fda16d100fb
Nguyen-Son-Tung <nstung.dev@gmail.com>
2026-03-23 10:39:50 +0700

    Add JsonSerializable for API Gateway types
    Register APIGatewayHttpApiV2ProxyRequest/Response with System.Text.Json source generator and add Amazon.Lambda.APIGatewayEvents package. This enables compile-time JSON serialization support for API Gateway proxy request/response types used by the Lambda-hosted API (package added to csproj: Amazon.Lambda.APIGatewayEvents v2.7.3).

--- src/FinanceTelegramBot.API/AppJsonSerializerContext.cs
+++ src/FinanceTelegramBot.API/AppJsonSerializerContext.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Text.Json.Serialization;
+using Amazon.Lambda.APIGatewayEvents;
 using FinanceTelegramBot.Core.Models.Requests;
 using FinanceTelegramBot.Core.Models.Responses;
 
 namespace FinanceTelegramBot.API;
 
+[JsonSerializable(typeof(APIGatewayHttpApiV2ProxyRequest))]
+[JsonSerializable(typeof(APIGatewayHttpApiV2ProxyResponse))]
 [JsonSerializable(typeof(TelegramSendMessageRequest))]
 [JsonSerializable(typeof(TelegramWebhookUpdateRequest))]
 [JsonSerializable(typeof(TelegramMessage))]
